7dee015f32a3c02d558fc292bbe4e3dddd367e07
BigForLy <oringan@mail.ru>
2021-09-08 17:15:15 +0500

    #5

--- app/src/main/java/com/example/myapplication/FrameManager.kt
+++ app/src/main/java/com/example/myapplication/FrameManager.kt
@@ -1,11 +1,37 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import com.example.myapplication.ui.Home.HomeFragment
+import com.example.myapplication.ui.Town.TownFragment
 
 class FrameManager : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val townFragment = TownFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame_manager)
+        replaceFragment(homeFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home-> replaceFragment(homeFragment)
+                R.id.town -> replaceFragment(townFragment)
+            }
+            true
+        }
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
     }
 }
